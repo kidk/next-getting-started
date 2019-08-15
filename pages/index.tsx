b99f56526da0249781f5ba9cd03ceec2266889c0
Keerthikan Thurairatnam <keerthikan.thurairatnam@scout24.ch>
2019-08-15 19:52:27 +0200

    Add publicRuntimeConfig to next config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
+import getConfig from 'next/config';
+
+const { publicRuntimeConfig } = getConfig();
 
 class IndexPage extends React.Component {
     render() {
-        return <span>hello</span>
+        return <span>hello {publicRuntimeConfig.hello}</span>
     }
 }
 
