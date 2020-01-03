c0b7fe0f34979170ea0bac47b0e5903e6b7134cb
tk-codes <kk.devpro11@gmail.com>
2020-01-03 12:19:51 +0100

    setup newrelic for custom instrumentation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react';
 import { API_URL } from '../src/api';
+import { startSegment } from 'newrelic';
 
 class IndexPage extends React.Component {
     render() {
+        startSegment('render/IndexPage', true, () => {
+            return;
+        });
+
         return <span>hello {API_URL}</span>
     }
 }
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('newrelic');
 const express = require("express");
 const next = require("next");
 const getConfig = require("next/config");
