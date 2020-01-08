2c6fcd3fdc17888beeaabc4abc7ebf6c545d94e2
Samuel Vandamme <samuel@sava.be>
2020-01-08 16:24:59 +0000

    Fixed compile issue and agent is reporting to back-end

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,8 @@
 import * as React from 'react';
 import { API_URL } from '../src/api';
-import { startSegment } from 'newrelic';
 
 class IndexPage extends React.Component {
     render() {
-        startSegment('render/IndexPage', true, () => {
-            return;
-        });
-
         return <span>hello {API_URL}</span>
     }
 }
