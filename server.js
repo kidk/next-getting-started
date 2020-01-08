7a63be197634fa053ffc465fa14b1fbc052e1a5a
Samuel Vandamme <samuel@sava.be>
2020-01-08 16:30:22 +0000

    Added New Relic page tracking example

--- server.js
+++ server.js
@@ -10,6 +10,16 @@ const handler = app.getRequestHandler();
 app.prepare().then(() => {
     server.use(handler);
 
+    server.get('*', (req, res) => {
+        const parsedUrl = parse(req.url, true)
+        const { pathname, query } = parsedUrl
+        // Transaction Name: (w/o this new relic shows "/*" for all transaction)
+        // https://docs.newrelic.com/docs/agents/nodejs-agent/api-guides/guide-using-nodejs-agent-api
+        newrelic.setTransactionName(pathname);
+
+        return handle(req, res)
+    });
+
     server.listen(3001, err => {
         console.log('listening on 3001');
     });
